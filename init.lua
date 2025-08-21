dc358bf39e73740a912c227f7e9057c3ee50477c
Angel Gomez Salazar <angel.edj.gs@gmail.com>
2025-08-21 12:58:01 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -34,15 +34,77 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.shiftround = false
+vim.opt.termguicolors = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     {"neanias/everforest-nvim", version = false, lazy = false, priority = 1000},
-    'neovim/nvim-lspconfig',
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
+    {
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+      },
+      config = function()
+        -- Setup nvim-cmp (minimal)
+        local cmp = require('cmp')
+        cmp.setup({
+          mapping = cmp.mapping.preset.insert({
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<Tab>'] = cmp.mapping.select_next_item(),
+            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          }),
+          sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'buffer' },
+          }),
+        })
+
+        -- Setup LSP capabilities for autocomplete
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lspconfig = require('lspconfig')
+
+        -- Common on_attach function for all LSPs
+        local on_attach = function(client, bufnr)
+          local opts = { noremap = true, silent = true, buffer = bufnr }
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        end
+
+        -- Rust (rust-analyzer)
+        lspconfig.rust_analyzer.setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+          settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = {
+                command = "clippy"
+              },
+            }
+          }
+        })
+
+        -- Python (pyright)
+        lspconfig.pyright.setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
+
+        -- TypeScript/JavaScript (ts_ls)
+        lspconfig.ts_ls.setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
+      end,
+    },
     'nvim-lua/plenary.nvim',
     'jose-elias-alvarez/null-ls.nvim',
     {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
@@ -55,7 +117,7 @@ require("lazy").setup({
     {
       "greggh/claude-code.nvim",
       dependencies = {
-        "nvim-lua/plenary.nvim", -- Already included above, but good to be explicit
+        "nvim-lua/plenary.nvim",
       },
       config = function()
         require("claude-code").setup({
